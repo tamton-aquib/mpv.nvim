72fd426c7c9ceb41ffc8238ac72455ea4774bfd9
Tamton Aquib <aquibjavedt007@gmail.com>
2024-03-21 12:47:48 +0530

    feat: thumbnails

--- lua/mpv/actions.lua
+++ lua/mpv/actions.lua
@@ -2,11 +2,19 @@ local M = {}
 
 M.left_mouse = function(state, conf)
     local mouse = vim.fn.getmousepos()
-    if M.win ~= mouse.winid then return end
+    if state.win ~= mouse.winid then return end
 
-    local pause = math.floor(conf.width/2)
-    local prev = math.floor(conf.width/4)
-    local next = math.floor(3 * (conf.width/4))
+    local image_width = state.image.rendered_geometry and state.image.rendered_geometry.width or 0
+    print("conf width: ", conf.width)
+    print("image width: ", image_width)
+    print("win width: ", vim.api.nvim_win_get_config(0).width)
+    -- local pause = math.floor(conf.width/2)
+    -- local prev = math.floor(conf.width/4)
+    -- local next = math.floor(3 * (conf.width/4))
+    local pause = math.floor((conf.width - image_width)/2) + image_width
+    local prev = math.floor((conf.width - image_width)/4) + image_width
+    local next = math.floor(3 * ((conf.width - image_width)/4)) + image_width
+    print("Pause: ", pause)
     if (mouse.winrow-1) == conf.height and math.abs(pause - mouse.wincol) < 3 then
         state.paused = not state.paused
         vim.api.nvim_chan_send(state.jobid, 'p')
--- lua/mpv/init.lua
+++ lua/mpv/init.lua
@@ -4,9 +4,10 @@
 --- p: pause/play         q: quit                      m: mute
 --- >: next               <: prev (in playlists)
 
-local M = {buf=nil, win=nil, ns=vim.api.nvim_create_namespace("mpv"), content_id=nil, title_id=nil}
-local conf = { width=50, height=5, border='single', setup_widgets=false, timer={after=1000, throttle=250} }
-local state = {playing=false, jobid=nil, title=nil, paused=false, timing="", percent=0, muted=false, loaded=false}
+local border = { { "▁", "None" }, { "▁", "None" }, { "▁", "None" }, { "▕", "NormalFloat" }, { "▔", "None" }, { "▔", "None" }, { "▔", "None" }, { "▏", "NormalFloat" } }
+local M = {win=nil, ns=vim.api.nvim_create_namespace("mpv"), content_id=nil, title_id=nil}
+local conf = {width=50, height=5, border= border, setup_widgets=false, timer={after=1000, throttle=250}}
+local state = {thumbnail = nil, buf=nil, playing=false, jobid=nil, title=nil, paused=false, timing="", percent=0, muted=false, loaded=false, image=nil}
 local win_opts = {relative='editor', style='minimal', border=conf.border, row=1, col=vim.o.columns-conf.width-2, height=conf.height, width=conf.width } -- , title='Mpv', title_pos='center' }
 local hls = {title="String", timer="Identifier", progress="Function"}
 local queue = {}
@@ -16,31 +17,56 @@ local actions = require("mpv.actions")
 -- NOTE: for statusline components
 M.music_info = function() return state end
 
-local by3 = (" "):rep(math.floor(conf.width/4)-1)
+local by3 = function()
+    local w = 0
+    if state.image then
+        w = conf.width - (state.image.rendered_geometry.width or 0)
+        return (" "):rep(math.floor(w/4)-1)
+    else
+        return (" "):rep(math.floor(conf.width/4)-1)
+    end
+end
+
 local refresh_screen = function()
     if not state.loaded then return end
 
     local chars = { "󰽰" }
     local char = chars[math.random(#chars)]
+    local image_width = 0
+    if state.image then
+        if state.image.rendered_geometry then
+            image_width = state.image.rendered_geometry.width + 1
+        end
+    end
+    local iw = (" "):rep(image_width)
 
     local dur = math.floor((state.percent/100) * conf.width)
-    vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
-        virt_text = {{state.title or (#queue >= 0 and queue[1] or 'Not Playing'), hls.title}}, virt_text_pos='overlay',
+    vim.api.nvim_buf_set_extmark(state.buf, M.ns, 0, 0, {
+        virt_text = {
+            {
+                (state.title or (#queue >= 0 and queue[1] or 'Not Playing')), hls.title
+            }
+        },
+        virt_text_pos = 'overlay',
         id = M.title_id
     })
 
     local time1, time2 = unpack(vim.split(state.timing, ' / '))
-    vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
+    vim.api.nvim_buf_set_extmark(state.buf, M.ns, 0, 0, {
         virt_lines = {
             {
-                {time1, hls.timer}, {(" "):rep(conf.width - 16)}, {time2, hls.timer}
+                {iw .. time1, hls.timer}, {(" "):rep(conf.width - 16 - image_width)}, {time2, hls.timer}
             },
             {
-                {("▁"):rep(dur), hls.progress}, {char, hls.progress}, {("▁"):rep(conf.width-dur), "Comment"}
+                {iw .. ("▁"):rep(dur), hls.progress},
+                {char, hls.progress},
+                {("▁"):rep(conf.width - dur - image_width), "Comment"}
             },
             {{"", ""}},
             {
-                {by3.."󰞓 ", hls.progress}, {by3..(not state.paused and "" or ""), hls.progress}, {by3.."󰞔 ", hls.progress}
+                {iw .. by3() .."󰞓 ", hls.progress},
+                {by3() ..(not state.paused and "" or ""), hls.progress},
+                {by3() .."󰞔 ", hls.progress}
             }
         },
         id = M.content_id
@@ -49,17 +75,37 @@ end
 
 local function play_song(query)
     local command = { "mpv", "--term-playing-msg='${media-title}'", "--no-video", query }
+    local image_width
+    if state.thumbnail then
+        -- require("image").from_url("https://img.youtube.com/vi/"..current_thumbnail.."/0.jpg", {}, function(image)
+        -- require("image").from_url("https://img.youtube.com/vi/"..current_thumbnail.."/mqdefault.jpg", {}, function(image)
+        require("image").from_url("https://i.ytimg.com/vi/"..state.thumbnail.."/mqdefault.jpg", {}, function(image)
+            state.image = image
+            local wo = vim.api.nvim_win_get_config(state.win)
+            image:render({
+                window = state.win,
+                buffer = state.buf,
+                x = wo.col, y = wo.row + 1,
+                height = 5,
+            })
+
+            image_width = image.rendered_geometry.width + 1
+            wo["width"] = wo["width"] + image_width
+            image:move(wo.col - image_width, wo.row + 1)
+            conf['width'] = conf['width'] + image_width
+            vim.api.nvim_win_set_config(state.win, wo)
+        end)
+    end
     state.jobid = vim.fn.jobstart(command, {
         pty = true,
         on_stdout = function(_, data)
             if data then
-                -- vim.print(vim.api.nvim_list_bufs())
                 local time = data[1]:match([[%d%d:%d%d:%d%d / %d%d:%d%d:%d%d]])
                 local percent = data[1]:match([[(%d%d?%%)]])
                 if percent then state.percent = percent:sub(1, -2) end
 
                 if not data[1]:match("A:") and not data[1]:match("AO:") then
-                    state.title = data[1]:match([['(.*)']]) or state.title
+                    state.title = (" "):rep(image_width or 0) .. (data[1]:match([['(.*)']]) or state.title or "")
                 end
 
                 if time then
@@ -74,6 +120,7 @@ local function play_song(query)
         on_exit = function()
             state.playing = false
             state.title = nil
+            if state.image then state.image:clear() end
             refresh_screen()
 
             if #queue > 0 then
@@ -92,46 +139,69 @@ local map = function(bind, to, fn)
             if fn then fn() end
             refresh_screen()
         end
-    end, { buffer = M.buf })
+    end, { buffer = state.buf })
 end
 
 
 local function ask_input(query)
-        if query == "" then vim.notify("Query not provided!") return end
-        M.title_id = vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, { virt_text = {{'Searching for "'..query..'"...', hls.title}}, virt_text_pos='overlay', id=M.title_id })
-        if not query:match([[^https://.*youtube.com]]) then query = "ytdl://ytsearch:"..table.concat(vim.split(query, ' '), '+') end
+    if query == "" then vim.notify("Query not provided!") return end
+    local image_width = 0
+    if state.image then image_width = state.image.rendered_geometry.width end
+    M.title_id = vim.api.nvim_buf_set_extmark(
+        state.buf,
+        M.ns,
+        0, 0,
+        {
+            virt_text = {
+                { (" "):rep(image_width) .. 'Searching for "'.. query ..'"...', hls.title }
+            },
+            virt_text_pos = 'overlay',
+            id = M.title_id
+        }
+    )
 
-        -- Examples for small music from youtube
-        -- https://www.youtube.com/watch?v=0yZcDeVsj_Y
-        -- https://www.youtube.com/watch?v=FUKmyRLOlAA
-        if not state.playing then
-            play_song(query)
-        else
-            table.insert(queue, query)
-        end
+	if not query:match([[^https://.*youtube.com]]) then
+		---@diagnostic disable-next-line: undefined-field
+		if not vim.uv.fs_stat(query) then
+			query = "ytdl://ytsearch:"..table.concat(vim.split(query, ' '), '+')
+		end
+    end
+
+    state.thumbnail = query:match("v=([a-zA-Z0-9_]*)")
+
+    -- Examples for small music from youtube
+    -- https://www.youtube.com/watch?v=0yZcDeVsj_Y
+    -- https://www.youtube.com/watch?v=FUKmyRLOlAA
+    if not state.playing then
+        play_song(query)
+    else
+        vim.notify("Song added to queue!")
+        table.insert(queue, query)
+    end
 end
 
 M.toggle_player = function()
-    if state.loaded and vim.api.nvim_win_is_valid(M.win) then
-        pcall(vim.api.nvim_buf_delete, M.buf, {force=true})
-        pcall(vim.api.nvim_win_close, M.win, true)
+    if state.loaded and vim.api.nvim_win_is_valid(state.win) then
+        pcall(vim.api.nvim_buf_delete, state.buf, {force=true})
+        pcall(vim.api.nvim_win_close, state.win, true)
+        if state.image then state.image:clear() end
         state.loaded = false
         return
     end
 
-    M.buf = vim.api.nvim_create_buf(false, true)
-    vim.bo[M.buf].filetype = 'mpv'
-    M.win = vim.api.nvim_open_win(M.buf, true, win_opts)
+    state.buf = vim.api.nvim_create_buf(false, true)
+    vim.bo[state.buf].filetype = 'mpv'
+    state.win = vim.api.nvim_open_win(state.buf, true, win_opts)
 
     -- Title for mpv window
-    M.title_id = vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
+    M.title_id = vim.api.nvim_buf_set_extmark(state.buf, M.ns, 0, 0, {
         virt_text = {
             {state.title or (#queue >= 0 and queue[1] or 'Not Playing'), hls.title}
         }, virt_text_pos = 'overlay'
     })
 
     -- Contents inside mpv window
-    M.content_id = vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
+    M.content_id = vim.api.nvim_buf_set_extmark(state.buf, M.ns, 0, 0, {
         virt_lines = {
             -- Time progress / Total time
             { {state.timing, hls.timer} },
@@ -140,13 +210,14 @@ M.toggle_player = function()
             -- Empty line
             {{"", ""}},
             -- Controls on the window
-            { {by3.."󰞓 ", hls.progress}, {by3..(state.paused and "" or ""), hls.progress}, {by3.."󰞔 ", hls.progress} }
+            { {by3() .."󰞓 ", hls.progress}, {by3() ..(state.paused and "" or ""), hls.progress}, {by3() .."󰞔 ", hls.progress} }
         },
     })
+    -- if img then img:render() end
 
     vim.keymap.set({'n', 'i'}, '<CR>', function()
         vim.ui.input({ width = 40 }, ask_input)
-    end, { buffer = M.buf })
+    end, { buffer = state.buf })
 
     map('q', 'q', function()
         state.title = 'Not Playing'
@@ -160,7 +231,7 @@ M.toggle_player = function()
     map('<', '<')
     map('<LeftMouse>', '',
         function()
-            actions.left_mouse()
+            actions.left_mouse(state, conf)
             refresh_screen()
         end
     )
